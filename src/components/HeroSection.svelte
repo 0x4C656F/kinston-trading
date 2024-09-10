245dfdff3fb0a4da8ede1ebda729b0ce8206d50f
Lev Lavryniuk <leoyt109@gmail.com>
2024-09-10 14:52:18 +0200

    working

--- src/components/HeroSection.svelte
+++ src/components/HeroSection.svelte
@@ -2,10 +2,12 @@
 	import Waves from './Waves.svelte';
 </script>
 
-<section class="bg-muted flex items-center min-h-screen justify-center w-full flex-col">
+<section
+	class="bg-muted flex items-center overflow-x-hidden min-h-screen justify-center w-full flex-col"
+>
 	<Waves />
 	<div
-		class="  items-center w-full h-[50vh] flex justify-start max-w-screen-2xl bg-transparent z-10"
+		class="items-center w-full h-[50vh] flex justify-around max-w-screen-2xl bg-transparent z-10"
 	>
 		<div class="space-y-4">
 			<h1 class="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl max-w-2xl">
@@ -19,5 +21,6 @@
 				unparalleled customer service.
 			</p>
 		</div>
+		<img src="images/hero.webp" alt="Hero" />
 	</div>
 </section>
